472299005189fdffaa268026a5978416b7577c6e
Alejandro Garcés-Ruiz <67441444+alejandrogarces@users.noreply.github.com>
2026-05-15 20:28:13 -0500

    Update flujodc.jl

--- Garver/flujodc.jl
+++ Garver/flujodc.jl
@@ -6,13 +6,13 @@ using JuMP
 using HiGHS
 
 # Parametros
-gmax = [150;  0;360;  0;  0;600]
+gmax = [150;  0;360;  0;  0;600]/100
 #gmax = [50; 0; 165; 0; 0; 600]
-d = [ 80;240; 40;160;240;  0]
+d = [ 80;240; 40;160;240;  0]/100
 lineas = [(1,2);(1,4);(1,5);(2,3);(2,4);(2,6);(3,5);(4,6)]
 c = [40;60;20;20;40;30;20;30]
 x = [0.4;0.6;0.2;0.2;0.4;0.3;0.2;0.3]
-fmax = [100;80;100;100;100;100;100;100]
+fmax = [100;80;100;100;100;100;100;100]/100
 z0 = [1;1;1;1;1;0;1;0]
 
 # triplicar las lineas para adminir mas líneas por corredor
@@ -48,13 +48,13 @@ ModeloDC = Model(HiGHS.Optimizer)
 @constraint(ModeloDC, f .>= -z.*fmax)
 @constraint(ModeloDC, g .<= gmax)
 @constraint(ModeloDC, z[1:ncor] == z0)
-@constraint(ModeloDC,-z*η <= x.*f/100)
-@constraint(ModeloDC, x.*f/100 .<= z*η )
+@constraint(ModeloDC,-z*η <= x.*f)
+@constraint(ModeloDC, x.*f .<= z*η )
 for (k,conexion) in enumerate(lineas)
     i = Int(conexion[1])
     j = Int(conexion[2])
-    @constraint(ModeloDC, -(1-z[k])*η <= x[k].*f[k]/100 - (θ[i]-θ[j]) )
-    @constraint(ModeloDC, x[k].*f[k]/100 - (θ[i]-θ[j]) <= (1-z[k])*η )
+    @constraint(ModeloDC, -(1-z[k])*η <= x[k].*f[k] - (θ[i]-θ[j]) )
+    @constraint(ModeloDC, x[k].*f[k] - (θ[i]-θ[j]) <= (1-z[k])*η )
 end
 optimize!(ModeloDC)
 
